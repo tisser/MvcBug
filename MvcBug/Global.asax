34d959bb52cbee391f39f73ef6c3ab493a7bdb40
prilatof <prilatof@gmail.com>
2014-06-10 13:35:38 +0400

    First Commit

---
+++ MvcBug/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcBug.MvcApplication" Language="C#" %>
---
+++ MvcBug/Models/StateRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBug.Models
+{
+    public class StateRepository
+    {
+    }
+}
